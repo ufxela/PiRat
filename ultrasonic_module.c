e29014bbb37639a0f0016afcad346ed853052b15
Alex Fu <afu625@stanford.edu>
2019-03-05 21:40:18 -0800

    working ultrasonic module. Realized that I don't need interrupts for this probably

--- ultrasonic_module.c
+++ ultrasonic_module.c
@@ -5,16 +5,18 @@
 #include "printf.h"
 #include "uart.h"
 
+const unsigned int TRIGGER = GPIO_PIN23;
+const unsigned int ECHO = GPIO_PIN24;
+
 void ultrasonic_init(unsigned int trig_pin, unsigned int echo_pin){
   gpio_set_output(trig_pin);
 
   gpio_set_input(echo_pin);
   gpio_set_pulldown(echo_pin);
-  gpio_enable_event_detection(echo_pin, GPIO_DETECT_RISING_EDGE);
 }
 
 /* a simple test function which gets the time of flight, doesn't use interrupts */
-int get_time_of_flight_basic(unsigned int trig_pin, unsigned int echo_pin){
+unsigned int get_time_of_flight_basic(unsigned int trig_pin, unsigned int echo_pin){
   timer_init();
 
   ultrasonic_init(trig_pin, echo_pin);
@@ -28,7 +30,7 @@ int get_time_of_flight_basic(unsigned int trig_pin, unsigned int echo_pin){
   /*this line is borrowed from Pat's code*/
   timer_delay_us(149); // wait til device settles: 148 = time to go one inch
   
-  //record time when echo is recieved
+  //wait for echo to be received
   while(gpio_read(echo_pin) == 0){
     /* spin */
   }
@@ -36,17 +38,27 @@ int get_time_of_flight_basic(unsigned int trig_pin, unsigned int echo_pin){
   while(gpio_read(echo_pin) == 1){
     /*spin */
   }
+
+  //record time
   unsigned int echo_time_us = timer_get_ticks();
 
+  //convert time into inches
   return (echo_time_us - trigger_time_us)/149;
+}
+
+unsigned int get_ultrasonic_mean(int number_samples){
+  unsigned int total = 0;
+  for(int i = 0; i < number_samples; i++){
+    total += get_time_of_flight_basic(TRIGGER, ECHO);
+    timer_delay_us(1000);
+  }
+  return total / number_samples;
 }
 
 int ultrasonic_test(void){
   while(1){
-    printf("time of flight in us: %d \n", get_time_of_flight_basic(GPIO_PIN23, GPIO_PIN24));
-    timer_delay_ms(100);
+    printf("time of flight in us: %d \n", get_time_of_flight_basic(TRIGGER, ECHO));
   }
-
   return 0;
 }
 
