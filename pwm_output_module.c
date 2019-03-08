149334704d87642a29262d90dfb5757ee5c360b1
Alex Fu <afu625@stanford.edu>
2019-03-07 16:50:07 -0800

    tried to make some major restructrual changes to pwm output. Not using a counter anymore, instead a big interrupt block

--- pwm_output_module.c
+++ pwm_output_module.c
@@ -1,5 +1,6 @@
-/* NOTE since I have a different pwm read and write file, may be nice if made functions names
- * more specific for output in this module
+/* a module for sending PWM signals to servos. Leverages the fact that duty cycles
+ * are between 5 and 10%
+ * uses interrupts. 
  */
 #include "gpio.h"
 #include "gpioextra.h"
@@ -15,78 +16,84 @@ const unsigned int MAX_PWM_OUTPUTS = 8;
 
 /* this data structure holds each of the pins which are available for use, and the "thresholds"
  * for each pin.
- * thresholds are when in the pwm cycle that we should output 1 and when we should do 0.
+ * thresholds are the marker, in microseconds, of when we should write 0 instead of 1 in the 
+ * pwm cycle.
  */
 volatile unsigned int number_of_pwm_outputs = 0;
 volatile unsigned int * pwm_output_pins; 
 volatile unsigned int * pwm_output_thresholds;
 
-//number of interrupts per pwm cycle & length of cycle in us
-unsigned int pwm_output_resolution = 0;
-unsigned int pwm_output_cycle_length_us = 0; //Q: is this variable necessary?
-
-//keeps track of which timer interrupt we're currently on, to compare against thresholds
-volatile unsigned int current_timer_interrupt_count = 0;
-
-/* is this time sensitive enough that we have to worry? servo cycle length is 20ms
- * but duty cycle is typically 5 - 10 % = 1 - 2 ms
- * if we have a resolution of 180, this means time between interrupts ~5 us
- * the pi runs at 700 mhz => 700 instructions per microsecond. Yeah I think we're good
- * as the handler only takes 100 instructions max I think
- * well I tried doing it with a resolution of 2000 and cycle length of 20000 and it did not work...
- * it would just halt after global interrupts were enabled.
- * dumming it down to 200 made it run. Hmm.
+/*TODO:
+ * WRITE A FUNCTION CALLED SET DUTY CYCLE which sets duty cycle
+ * or an angle function
+ * or a throttle function
+ * perhaps these are better to do in a different module that adds a layer of abstraction.
  */
+
+//TODO:
+//make functions to allow for these to be changed
+const unsigned int pwm_output_cycle_length_us = 4000000; //20 ms, for servos.
+const unsigned int pwm_output_max_threshold = 2000; //2ms, corresponds to max of 10% duty cycle
+const unsigned int pwm_output_min_threshold = 1000;
+
+//helper function which gets the index of a pin. -1 if not existent
+//TODO: use this function in this module
+static int pwm_output_get_pin_index(unsigned int pin){
+  for(int i = 0; i < number_of_pwm_outputs; i++){
+    if(pwm_output_pins[i] == pin){
+      return i;
+    }
+  }
+  return -1; 
+}
+
 static bool pwm_output_handler(unsigned int pc){
   //check if we cause the interrupt:
   if(armtimer_check_interrupt()){
-    //increment timer interrupt count
-    current_timer_interrupt_count++;
+    printf("in handler. Time: %d\n", timer_get_ticks());
+    //get the current time. QUESTION Is this limited by the 35 minute wraparound?
+    unsigned int start_time = timer_get_ticks();
 
-    //if cycle is finished, wraparound
-    if(current_timer_interrupt_count >= pwm_output_resolution){ 
-      current_timer_interrupt_count = 0;
-      //write 1 to each output, to starty cycle
-      for(int i = 0; i < number_of_pwm_outputs; i++){
-	gpio_write(pwm_output_pins[i], 1);
-      }
-    }
-
-    //for each pwm output
+    //write 1 to all servo channels to start duty cycle
     for(int i = 0; i < number_of_pwm_outputs; i++){
-      //write 0 when we reach the threshold
-      if(pwm_output_thresholds[i] == current_timer_interrupt_count){
-	gpio_write(pwm_output_pins[i], 0);
+      gpio_write(pwm_output_pins[i], 1);
+      printf("written 1 to pin %d, time %d\n", pwm_output_pins[i], timer_get_ticks());
+    }
+    
+    //loop through and set output to zero once threshold is reached
+    unsigned int current_time = timer_get_ticks() - start_time;
+    //at 2ms mark, we're done
+    while(current_time < pwm_output_max_threshold){
+      current_time = timer_get_ticks() - start_time; 
+      //check thresholds
+      for(int i = 0; i < number_of_pwm_outputs; i++){
+	//check if we should kill the output
+	if(pwm_output_thresholds[i] <= current_time){
+	  gpio_write(pwm_output_pins[i], 0); //this is writing 0 an unnecessary large number of times...
+	  printf("written 0 to pin %d, time %d\n", pwm_output_pins[i], timer_get_ticks());
+	}
       }
     }
 
+    //we're almost done: just make sure everything is turned off
+    for(int i = 0; i < number_of_pwm_outputs; i++){
+      gpio_write(pwm_output_pins[i], 0);
+    }
+    printf("exiting handler, time: %d\n", timer_get_ticks()); 
     return true;
   }else{
     return false;
   }
 }
 
-//will calculate the time between interrupts, and then setup interrupts to trigger then
-//Q: should I have checks for the parameters? also should I have default values?
-void pwm_output_init(unsigned int interrupts_per_cycle, unsigned int cycle_length_in_us){
+//setup everything
+void pwm_output_init(){
   //setup data structure
-  //Q: function for this?
   pwm_output_pins = malloc(4 * number_of_pwm_outputs);
   pwm_output_thresholds = malloc(4 * number_of_pwm_outputs);
 
-  pwm_output_resolution = interrupts_per_cycle;
-  pwm_output_cycle_length_us = cycle_length_in_us;
-
-  //calculate the time between each interrupt
-  //hopefully cycle_length_in_us is a multiple of interrupts_per_cycle
-  unsigned int time_between_pwm_output_interrupts = cycle_length_in_us / interrupts_per_cycle; 
-
-  //remove this later, it's to manually set the timer interrupt for every 10 us
-  time_between_pwm_output_interrupts = 10;
-  pwm_output_resolution = 6700;
-
   //initialize interrupts 
-  armtimer_init(time_between_pwm_output_interrupts);
+  armtimer_init(pwm_output_cycle_length_us);
   armtimer_enable();
   armtimer_enable_interrupts();
   interrupts_enable_basic(INTERRUPTS_BASIC_ARM_TIMER_IRQ);
@@ -94,16 +101,11 @@ void pwm_output_init(unsigned int interrupts_per_cycle, unsigned int cycle_lengt
   interrupts_global_enable();
 }
 
-int get_resolution(void){
-  return pwm_output_resolution;
-}
-
 int get_number_pwm_outputs(void){
   return number_of_pwm_outputs;
 } 
 
-/* THIS FUNCTION ALSO NEEDS TO ENABLE GPIO OUTPUT LOL*/
-int pwm_add_output(unsigned int pin, unsigned int starting_duty_cycle){
+int pwm_add_output(unsigned int pin, unsigned int starting_threshold){
   if(number_of_pwm_outputs < MAX_PWM_OUTPUTS){
     //make more room in data structure
     pwm_output_pins = realloc((void *) pwm_output_pins, 4 * (number_of_pwm_outputs + 1));
@@ -114,7 +116,7 @@ int pwm_add_output(unsigned int pin, unsigned int starting_duty_cycle){
 
     //initialize data
     pwm_output_pins[number_of_pwm_outputs] = pin;
-    pwm_output_thresholds[number_of_pwm_outputs] = starting_duty_cycle;
+    pwm_output_thresholds[number_of_pwm_outputs] = starting_threshold;
 
     //update number of pwm outputs, done at end to avoid messyness is interrupt happens in the middle
     number_of_pwm_outputs++;
@@ -132,13 +134,12 @@ int pwm_remove_output(unsigned int pin){
 /* BIG NOTE: should this function wait until the current cycle is completed before changing
  * the duty cycle? Probably
  */
-int pwm_change_duty_cycle(unsigned int pin, unsigned int new_duty_cycle){
-  if(new_duty_cycle < get_resolution()){
-    while(current_timer_interrupt_count != 0){ /* wait for next cycle to begin */}
+int pwm_change_threshold(unsigned int pin, unsigned int new_threshold){
+  if(new_threshold >= pwm_output_min_threshold && new_threshold <= pwm_output_max_threshold){
     //loop over, find pin, change threshold. 
     for(int i = 0; i < get_number_pwm_outputs(); i++){
       if(pwm_output_pins[i] == pin){
-	pwm_output_thresholds[i] = new_duty_cycle;
+	pwm_output_thresholds[i] = new_threshold;
 	break; //ugly, but safes unnecessary looping
       }
     }
@@ -150,31 +151,31 @@ int pwm_change_duty_cycle(unsigned int pin, unsigned int new_duty_cycle){
   }
 }
 
-int get_duty_cycle(unsigned int pin){
-  for(int i = 0; i < get_number_pwm_outputs(); i++){
-    if(pwm_output_pins[i] == pin){
-      return pwm_output_thresholds[i];
-    }
+int get_threshold(unsigned int pin){
+  int pin_index = pwm_output_get_pin_index(pin);
+  if(pin_index >= 0){
+    return pwm_output_thresholds[pin_index];
+  }else{
+    return -1;
   }
-  return -1; //not found
 }
 
 int pwm_output_test(void){
-  //add a servo on signal line 4, default duty cycle is 6%
-  pwm_add_output(GPIO_PIN4, 6800 * 70 / 1000);
-  //the code freezes at the above line.
+  printf("starting pwm output test\n");
+  pwm_add_output(GPIO_PIN4, 1200);
+  printf("output added");
   while(1){
-    //set duty cycle to 6%
-    pwm_change_duty_cycle(GPIO_PIN4, 6800 * 60 / 1000);
-    pwm_output_thresholds[0] = 6800;
-
-    //wait
+    printf("in loop \n");
+    pwm_change_threshold(GPIO_PIN4, 1200);
+    printf("1");
     timer_delay_ms(100);
-
-    //set duty cycle to 9 %
-    pwm_change_duty_cycle(GPIO_PIN4, 6800 * 90 /1000);    
-
-    //wait
-    timer_delay(100);
+    printf("2");
+    pwm_change_threshold(GPIO_PIN4, 1700);
+    printf("3");
+    timer_delay_ms(100);
+    printf("GPIO_PIN%d threshold: %d", GPIO_PIN4, get_threshold(GPIO_PIN4));
+    printf("beginning next iteration");
   }
+  return 1;
 }
+
--- pwm_output_module.h
+++ pwm_output_module.h
@@ -2,18 +2,17 @@
 #define PWM_OUTPUT_MODULE_H
 
 
-/* sets up interrupts and all as well as # of interrupts per pwm cycle and the length of the cycle
- * interrupts_per_cycle is basically the resolution
+/* sets up interrupts and all and the length of the cycle
  * Basically sets the frequency and resolution of pwm signals. TODO: write functions which allow
- * user to change frequency and resolution!
+ * user to change time between interrupts and time spent in interrupts!
  */
-void pwm_output_init(unsigned int interrupts_per_cycle, unsigned int cycle_length_in_us);
+void pwm_output_init();
 
 /* adds another pwm output source, located at pin pin and with a duty cycle
- * starting_duty_cycle is an int from 0 to interrupts_per_cycle, which is the resolution
+ * starting_duty_cycle is an int from 0 to 2000
  * returns 1 for success, 0 otherwise
  */
-int pwm_add_output(unsigned int pin, unsigned int starting_duty_cycle);
+int pwm_add_output(unsigned int pin, unsigned int starting_threshold);
 
 /* removes an output 
  */
@@ -22,17 +21,12 @@ int pwm_remove_output(unsigned int pin);
 /* changes duty cycle of existing pwm output 
  * returns 1 for success, 0 otherwise
  */
-int pwm_change_duty_cycle(unsigned int pin, unsigned int new_duty_cycle);
+int pwm_change_threshold(unsigned int pin, unsigned int new_threshold);
 
-/* returns the duty cycle of a pin, measured out of 0 from interrupts_per_cycle (resolution
+/* returns the duty cycle of a pin, measured from o to 2000. 
  * -1 if error
  */
-
-/* returns the duty cycle, as simple as it gets */
-int get_duty_cycle(unsigned int pin);
-
-/* returns the PWM resolution for each cycle */
-int get_resolution(void);
+int get_threshold(unsigned int pin);
 
 //a test file
 int pwm_output_test(void);
