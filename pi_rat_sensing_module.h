f83f67474cfbfdaa785c03c3f5109b9fd9e37eaf
Alex Fu <afu625@stanford.edu>
2019-03-12 19:50:56 -0700

    outlined what the pi_rat_sensing module will look like

---
+++ pi_rat_sensing_module.h
@@ -0,0 +1,20 @@
+#ifndef PI_RAT_SENSING_MODULE_H
+#define PI_RAT_SENSING_MODULE_H
+
+/* sets up ultrasonic sensor, line follower and panning servo */
+void pi_rat_sensing_module_init(unsigned int trigger_pin, unsigned int echo_pin, unsigned int servo_pin);
+
+/* determines if there is a wall to the left, center and right and returns a value
+ * corresponding to it
+ * The return value is a 3 bit number, with LSB representing left wall and MSB representing right wall
+ * 
+ * ex) return of 5 = 101 means there's a left wall, right wall and no center wall
+ */
+int pi_rat_get_walls();
+
+/* gets the current position of the line under the Pi Rat 
+ */
+int pi_rat_line_position();
+
+
+#endif
