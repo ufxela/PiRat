2382eb147e9738cc8efd01d6b47b32fad779aaea
Alex Fu <afu625@stanford.edu>
2019-03-17 18:49:39 -0700

    outlined the control module, which will have a solve maze function

---
+++ pi_rat_control.h
@@ -0,0 +1,12 @@
+#ifndef PI_RAT_CONTROL_H
+#define PI_RAT_CONTROL_H
+
+/* initializer function, sets up pi rat */
+void pi_rat_init();
+
+/* place the Pi at a given location and bearing, and ask it to solve a maze */
+void pi_rat_solve_maze(int x_start, int y_start, int bearing, int maze_width, int maze_height){
+
+}
+
+#endif
