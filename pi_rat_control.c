f350130613e5ef03399c95cb8e3102acd579a505
Alex Fu <afu625@stanford.edu>
2019-03-17 19:39:04 -0700

    wrote a few functions

--- pi_rat_control.c
+++ pi_rat_control.c
@@ -1,12 +1,31 @@
 #include "pi_rat_control.h"
 
+struct maze_node{
+  int visited; //0 for no 1 for yes
+  int left; //only makes sense if visited is 1. 1 for wall, 0 for no wall
+  int up;
+  int right;
+  int down;
+}
+
 /* needs some internal data structure to keep track of the Pi Rat's path */
+/* path stores as an array the moves made. 0 is go left, 1 is go forwards, 2 go right, 3 go back */
+int * path;
+int path_length;
 
 /* needs another internal data structure to keep track of the known elements of the maze 
  * 
  * Should be a 2d array of ints, with 1 representing a wall, 0 representing no wall
  * and -1 representing unknown. 
  */
+
+/* 2D array of maze_nodes. Very similar to frame buffer 2d array */
+maze_node** maze[][];
+
+int x_final;
+int y_final;
+int x_curr;
+int y_curr;
 
 void pi_rat_init(){
   
@@ -22,18 +41,28 @@ void pi_rat_solve_maze(int x_start, int y_start, int bearing, int x_end, int y_e
 }
 
 static void recursive_maze_solver(){
-  /* base case: we're at x_end, y_end. Then stop. */ 
+  if(x_curr == x_final && y_curr = y_final){
+    /* base case: we've solved the maze */
+  }else{ /* recursive case: explore, backtracking */
+    update_maze();
+    
 
-  /* get all possible moves */
+    /* get all possible moves */
+    
+    /* pick one possible move and perform it 
+     * This means both physically making the movement as well as recording it into the path
+     */
+    
+    /* unpick that move by reversing it as well as deleting it from the path */
 
-  /* pick one possible move and perform it 
-   * This means both physically making the movement as well as recording it into the path
-   */
 
-  /* unpick that move by reversing it as well as deleting it from the path */
-
-  /* repeat for all possible moves */
+    
+  }
 }
+
+/* gets surroundings and updates maze data structure */
+static void update_maze(){
+
 
 /* just wanders around the maze, without solving it yet, epsilon steps */
 void pi_rat_maze_wander(){
